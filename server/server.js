3de007e1a2a3ca751aba144874d13dd5d55c3f9d
Daniel <Shamaran85@gmail.com>
2019-03-31 23:57:05 +0200

    Added get/post endpoints

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const Data = require("./data");
 const { serverPort, dataBase } = require("../config");
 const app = express();
 
@@ -18,6 +19,40 @@ app.use(log("dev"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.post("/create", (req, res) => {
+  const shortId = Math.random()
+    .toString(36)
+    .substring(2, 8);
+
+  const data = new Data({
+    shortId: shortId,
+    url: req.body.url
+  });
+
+  data
+    .save()
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => console.log(err));
+
+  res.status(201).json({
+    data: data
+  });
+});
+
+app.get("/:shortId", (req, res) => {
+  Data.findOne({ shortId: req.params.shortId })
+    .then(doc => {
+      console.log(doc);
+      res.status(200).redirect(doc.url);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
 
 app.listen(serverPort, () =>
   console.log(`Server listening on port: ${serverPort}`)

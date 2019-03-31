c14a8066637c8fa25a199bd3f4622f3718a5d95d
Daniel <35291463+Shamaran85@users.noreply.github.com>
2019-04-01 00:35:46 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const serverPort = 3001;
-
-// const dataBase = "mongodb://tretton:tretton37@ds229186.mlab.com:29186/url-shortener";
-const dataBase = "mongodb://localhost:27017/trettondb";
-
-module.exports = {
-  serverPort,
-  dataBase
-};
